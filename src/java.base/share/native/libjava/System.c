c19b4bd72b1e7794f98afbfd9167321c3f7bd8a7
Volker H. Simonis <volker.simonis@gmail.com>
2018-01-15 15:23:30 +0100

    SapMachine branding for 'java.vendor', 'java.vendor.url' and 'java.vendor.url.bug' system properties

--- src/java.base/share/native/libjava/System.c
+++ src/java.base/share/native/libjava/System.c
@@ -110,13 +110,13 @@ Java_java_lang_System_identityHashCode(JNIEnv *env, jobject this, jobject x)
 
 /* Third party may overwrite these values. */
 #ifndef VENDOR
-#define VENDOR "Oracle Corporation"
+#define VENDOR "SAP SE"
 #endif
 #ifndef VENDOR_URL
-#define VENDOR_URL "http://java.oracle.com/"
+#define VENDOR_URL "http://sapmachine.org"
 #endif
 #ifndef VENDOR_URL_BUG
-#define VENDOR_URL_BUG "http://bugreport.java.com/bugreport/"
+#define VENDOR_URL_BUG "https://github.com/SAP/SapMachine/issues
 #endif
 
 #ifdef JAVA_SPECIFICATION_VENDOR /* Third party may NOT overwrite this. */
