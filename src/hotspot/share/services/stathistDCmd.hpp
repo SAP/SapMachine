9455aa0ed1bae8264f17d9099f5974bc3b096dc5
Christoph Langer <christoph.langer@sap.com>
2019-03-02 12:18:28 +0100

    SapMachine #315: Fix license headers, whitespace and SapMachine tagging

--- src/hotspot/share/services/stathistDCmd.hpp
+++ src/hotspot/share/services/stathistDCmd.hpp
@@ -1,7 +1,6 @@
 /*
  * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2019, SAP SE.
- *
+ * Copyright (c) 2019, SAP SE. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +22,6 @@
  * questions.
  *
  */
-
 
 #ifndef HOTSPOT_SHARE_SERVICES_STATHIST_INTERNALS_HPP
 #define HOTSPOT_SHARE_SERVICES_STATHIST_INTERNALS_HPP
