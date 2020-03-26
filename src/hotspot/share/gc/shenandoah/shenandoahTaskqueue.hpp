c0bb098476a00d4ba00ac1c1161b286d3248c39d
shade <none@none>
2020-03-26 19:09:20 +0100

    8241673: Shenandoah: refactor anti-false-sharing padding Reviewed-by: rkennke

--- src/hotspot/share/gc/shenandoah/shenandoahTaskqueue.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahTaskqueue.hpp
@@ -25,6 +25,7 @@
 #define SHARE_GC_SHENANDOAH_SHENANDOAHTASKQUEUE_HPP
 #include "gc/shared/owstTaskTerminator.hpp"
 #include "gc/shared/taskqueue.hpp"
+#include "gc/shenandoah/shenandoahPadding.hpp"
 #include "memory/allocation.hpp"
 #include "runtime/atomic.hpp"
 #include "runtime/mutex.hpp"
@@ -270,9 +271,9 @@ typedef Padded<ShenandoahBufferedOverflowTaskQueue> ShenandoahObjToScanQueue;
 template <class T, MEMFLAGS F>
 class ParallelClaimableQueueSet: public GenericTaskQueueSet<T, F> {
 private:
-  DEFINE_PAD_MINUS_SIZE(0, DEFAULT_CACHE_LINE_SIZE, sizeof(volatile jint));
+  shenandoah_padding(0);
   volatile jint     _claimed_index;
-  DEFINE_PAD_MINUS_SIZE(1, DEFAULT_CACHE_LINE_SIZE, 0);
+  shenandoah_padding(1);
 
   debug_only(uint   _reserved;  )
 
