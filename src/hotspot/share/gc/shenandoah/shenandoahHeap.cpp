ad93c81331d2c0fff31b7b54d58e387ea468bdfc
shade <none@none>
2020-04-14 15:33:00 +0200

    8242625: Shenandoah: restore heap logging for Degenerated/Full cycles Reviewed-by: rkennke, zgu

--- src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
@@ -2631,7 +2631,7 @@ void ShenandoahHeap::entry_final_updaterefs() {
 
 void ShenandoahHeap::entry_full(GCCause::Cause cause) {
   static const char* msg = "Pause Full";
-  ShenandoahPausePhase gc_phase(msg);
+  ShenandoahPausePhase gc_phase(msg, true /* log_heap_usage */);
   EventMark em("%s", msg);
 
   ShenandoahGCPhase total_phase(ShenandoahPhaseTimings::total_pause);
@@ -2647,7 +2647,7 @@ void ShenandoahHeap::entry_full(GCCause::Cause cause) {
 void ShenandoahHeap::entry_degenerated(int point) {
   ShenandoahDegenPoint dpoint = (ShenandoahDegenPoint)point;
   const char* msg = degen_event_message(dpoint);
-  ShenandoahPausePhase gc_phase(msg);
+  ShenandoahPausePhase gc_phase(msg, true /* log_heap_usage */);
   EventMark em("%s", msg);
 
   ShenandoahGCPhase total_phase(ShenandoahPhaseTimings::total_pause);
