14696cbc5269c157cb96a8b3a0f8b55d672ba403
shade <none@none>
2020-05-11 18:33:50 +0200

    8244740: Shenandoah: rename ShenandoahNormalMode to ShenandoahSATBMode Reviewed-by: rkennke

--- test/hotspot/jtreg/gc/shenandoah/TestObjItrWithHeapDump.java
+++ test/hotspot/jtreg/gc/shenandoah/TestObjItrWithHeapDump.java
@@ -56,7 +56,7 @@ public class TestObjItrWithHeapDump {
         }
 
         String[][][] modeHeuristics = new String[][][] {
-             {{"normal"},  {"adaptive", "compact", "static", "aggressive"}},
+             {{"satb"},    {"adaptive", "compact", "static", "aggressive"}},
              {{"iu"},      {"adaptive", "aggressive"}},
              {{"passive"}, {"passive"}}
         };
