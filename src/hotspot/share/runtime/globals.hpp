fd492547ba4c7fbc9e1ad001e96bfc90aa057bf4
Ralf Schmelter <ralf.schmelter@sap.com>
2023-07-26 14:12:31 +0200

    SapMachine #1449: Store vitals samples for max/min values
    (cherry picked from commit ee6966d6266e6b8d0c3743f0bfb9c7393a695fae)

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -523,6 +523,10 @@ const int ObjectAlignmentInBytes = 8;
   product(bool, PrintVitalsAtExit, false,                                   \
           "Prints vitals at VM exit to tty.")                               \
                                                                             \
+  product(bool, StoreVitalsExtremas, true,                                  \
+          "If enabled we store the samples for extremum values of "         \
+          "selected types.")                                                \
+                                                                            \
   product(ccstr, VitalsFile, NULL,                                          \
           "When DumpVitalsAtExit is set, the file name prefix for the "     \
           "output files (default is sapmachine_vitals_<pid>).")             \
