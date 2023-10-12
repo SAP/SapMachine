6f5370da226e64843019581b76b672fb42887202
Ralf Schmelter <ralf.schmelter@sap.com>
2023-10-12 18:11:26 +0200

    Check for proper output

--- test/hotspot/jtreg/runtime/malloctrace/MallocHooksTest.java
+++ test/hotspot/jtreg/runtime/malloctrace/MallocHooksTest.java
@@ -8,6 +8,7 @@
 
 import java.io.File;
 
+import jdk.test.lib.JDKToolFinder;
 import jdk.test.lib.Platform;
 import jdk.test.lib.Utils;
 import jdk.test.lib.process.ProcessTools;
@@ -89,16 +90,47 @@ public class MallocHooksTest {
         new OutputAnalyzer(pb.start()).shouldHaveExitValue(0);
     }
 
-    private static void testTracking() throws Exception {
-        ProcessBuilder pb = runStress(1, 1024 * 1024 * 100, 65536);
-        new OutputAnalyzer(pb.start()).shouldHaveExitValue(0);
+    private static long getPid(Process p) {
+        return p.pid();
     }
 
-    private static ProcessBuilder runStress(int runs, int nrOfOps, int maxLiveAllocations) {
-        return ProcessTools.createJavaProcessBuilder("-XX:+UseMallocHooks", "-XX:+MallocTraceAtStartup",
-                                                     "-XX:+MallocTraceTrackFrees", MallocHooksTest.class.getName(),
-                                                     "stress", "" + runs, "" + nrOfOps, 
-                                                     "" + maxLiveAllocations);
+    private static OutputAnalyzer callJcmd(Process p, String... args) throws Exception {
+        ProcessBuilder pb = new ProcessBuilder();
+        String[] realArgs = new String[args.length + 2];
+        System.arraycopy(args, 0, realArgs, 2, args.length);
+        realArgs[0] = JDKToolFinder.getJDKTool("jcmd");
+        realArgs[1] = Long.toString(getPid(p));
+        pb.command(realArgs);
+        return new OutputAnalyzer(pb.start());
+    }
+
+    private static void testTracking() throws Exception {
+        ProcessBuilder pb = runStress(10, 1024 * 1024 * 10, 65536, "-XX:-MallocTraceTrackFrees");
+        Process p = pb.start();
+        OutputAnalyzer oa = callJcmd(p, "MallocTrace.dump -max-entries=10");
+        oa.shouldHaveExitValue(0);
+        p.destroy();
+        oa.shouldContain("Contains every allocation done since enabling");
+        oa.shouldContain("libtestmallochooks.");
+        oa.shouldContain("Total allocated bytes");
+        oa.shouldContain("Total printed count");
+        oa.shouldContain("Stack 1 of");
+        oa.shouldContain("Stack 10 of");
+        oa.shouldNotContain("Stack 0 of");
+        oa.shouldNotContain("Stack 11 of");
+    }
+
+    private static ProcessBuilder runStress(int runs, int nrOfOps, int maxLiveAllocations, String... opts) {
+        String[] args = new String[opts.length + 7];
+        System.arraycopy(opts, 0, args, 0, opts.length);
+        args[opts.length + 0] = "-XX:+UseMallocHooks";
+        args[opts.length + 1] = "-XX:+MallocTraceAtStartup";
+        args[opts.length + 2] = MallocHooksTest.class.getName();
+        args[opts.length + 3] = "stress";
+        args[opts.length + 4] = Integer.toString(runs);
+        args[opts.length + 5] = Integer.toString(nrOfOps);
+        args[opts.length + 6] = Integer.toString(maxLiveAllocations);
+        return ProcessTools.createJavaProcessBuilder(args);
     }
 
     private static ProcessBuilder checkEnvProc(String env) {
