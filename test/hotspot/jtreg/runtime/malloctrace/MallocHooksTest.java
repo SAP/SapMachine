bab3e3d1f3fc024cdd3ed85d411c5d77c5ece943
Ralf Schmelter <ralf.schmelter@sap.com>
2023-10-26 18:04:47 +0200

    Make stress test reproducible and added code to parse the output

--- test/hotspot/jtreg/runtime/malloctrace/MallocHooksTest.java
+++ test/hotspot/jtreg/runtime/malloctrace/MallocHooksTest.java
@@ -6,7 +6,10 @@
  * @run main/othervm/native MallocHooksTest
  */
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.StringReader;
+import java.util.ArrayList;
 
 import jdk.test.lib.JDKToolFinder;
 import jdk.test.lib.Platform;
@@ -15,7 +18,7 @@ import jdk.test.lib.process.ProcessTools;
 import jdk.test.lib.process.OutputAnalyzer;
 
 public class MallocHooksTest {
-    static native void doRandomMemOps(int nrOfOps, int maxLiveAllocations);
+    static native void doRandomMemOps(int nrOfOps, int maxLiveAllocations, int seed);
 
     private static final String LD_PRELOAD = Platform.isOSX() ? "DYLD_INSERT_LIBRARIES" : "LD_PRELOAD";
     private static final String LIB_SUFFIX = Platform.isOSX() ? ".dylib" : ".so";
@@ -46,6 +49,10 @@ public class MallocHooksTest {
                                 LD_PRELOAD + "=\"" +  getLdPrelodEnv() + "\"");
         } else if (args[0].equals("stress")) {
             doStress(args);
+            System.out.println("Done");
+            while (true) {
+              Thread.sleep(1000);
+            }
         } else {
             throw new Exception("Unknown command " + args[0]);
         }
@@ -54,11 +61,9 @@ public class MallocHooksTest {
     private static void doStress(String[] args) {
         int nrOfOps = Integer.parseInt(args[1]);
         int maxLiveAllocations = Integer.parseInt(args[2]);
-        int runs = Integer.parseInt(args[3]);
+        int seed = Integer.parseInt(args[3]);
 
-        for (int i = 0; i < runs; ++i) {
-            doRandomMemOps(nrOfOps, maxLiveAllocations);
-        }
+        doRandomMemOps(nrOfOps, maxLiveAllocations, seed);
     }
 
     private static void testNoRecursiveCallsForFallbacks() throws Exception {
@@ -104,12 +109,23 @@ public class MallocHooksTest {
         return new OutputAnalyzer(pb.start());
     }
 
+    private static MallocTraceResult parseOutput(String output) throws Exception {
+       try (BufferedReader br = new BufferedReader(new StringReader(output))) {
+           return new MallocTraceResult(br);
+       }
+    }
+
     private static void testTracking() throws Exception {
-        ProcessBuilder pb = runStress(10, 1024 * 1024 * 10, 65536, "-XX:-MallocTraceTrackFrees", "-XX:MallocTraceStackDepth=2");
+        ProcessBuilder pb = runStress(1024 * 1024 * 10, 65536, 172369973, 
+                                      "-XX:-MallocTraceTrackFrees", "-XX:MallocTraceStackDepth=2");
         Process p = pb.start();
-        OutputAnalyzer oa = callJcmd(p, "MallocTrace.dump -max-entries=10");
+        p.getInputStream().read(); // Wait until we get some output, so we know the stress was run.
+        OutputAnalyzer oa = callJcmd(p, "MallocTrace.dump", "-max-entries=10", "-sort-by-count",
+                                        "-filter=Java_MallocHooksTest_doRandomMemOps");
         oa.shouldHaveExitValue(0);
         p.destroy();
+        System.out.println(oa.getOutput());
+        System.out.println(parseOutput(oa.getOutput()).toString());
         oa.shouldContain("Contains every allocation done since enabling");
         oa.shouldContain("libtestmallochooks.");
         oa.shouldContain("Total allocated bytes");
@@ -118,20 +134,19 @@ public class MallocHooksTest {
         oa.shouldContain("Stack 10 of");
         oa.shouldNotContain("Stack 0 of");
         oa.shouldNotContain("Stack 11 of");
-        System.out.println(oa.getOutput());
         throw new Exception();
     }
 
-    private static ProcessBuilder runStress(int runs, int nrOfOps, int maxLiveAllocations, String... opts) {
+    private static ProcessBuilder runStress(int nrOfOps, int maxLiveAllocations, int seed, String... opts) {
         String[] args = new String[opts.length + 7];
         System.arraycopy(opts, 0, args, 0, opts.length);
         args[opts.length + 0] = "-XX:+UseMallocHooks";
         args[opts.length + 1] = "-XX:+MallocTraceAtStartup";
         args[opts.length + 2] = MallocHooksTest.class.getName();
         args[opts.length + 3] = "stress";
-        args[opts.length + 4] = Integer.toString(runs);
-        args[opts.length + 5] = Integer.toString(nrOfOps);
-        args[opts.length + 6] = Integer.toString(maxLiveAllocations);
+        args[opts.length + 4] = Integer.toString(nrOfOps);
+        args[opts.length + 5] = Integer.toString(maxLiveAllocations);
+        args[opts.length + 6] = Integer.toString(seed);
         return ProcessTools.createJavaProcessBuilder(args);
     }
 
@@ -158,3 +173,130 @@ public class MallocHooksTest {
         System.loadLibrary("testmallochooks");
     }
 }
+
+class Stack {
+    private String[] funcs;
+    private int index;
+    private int maxIndex;
+    private long bytes;
+    private long count;
+
+    public Stack(BufferedReader reader, int index, int maxIndex, long bytes, long count) throws Exception {
+        this.index = index;
+        this.maxIndex = maxIndex;
+        this.bytes = bytes;
+        this.count = count;
+
+        ArrayList<String> lines = new ArrayList<>();
+
+        while (true) {
+            reader.mark(10);
+
+            if (reader.read() != ' ') {
+              reader.reset();
+              break;
+            }
+
+            lines.add(reader.readLine());
+        }
+
+        funcs = new String[lines.size()];
+
+        for (int i = 0; i < funcs.length; ++i) {
+            String line = lines.get(i);
+            int pos = line.indexOf(']');
+
+            if (pos > 0) {
+                funcs[i] = line.substring(pos + 1).trim();
+            } else {
+                funcs[i] = "<missing>";
+            }
+        }
+    }
+
+    public int getStackIndex() {
+        return index;
+    }
+
+    public int getMaxStackIndex() {
+        return maxIndex;
+    }
+
+    public long getBytes() {
+        return bytes;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public int getStackDepth() {
+        return funcs.length;
+    }
+    
+    public String getFunction(int index) {
+        return funcs[index];
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        result.append("Stack " + index + " of " + maxIndex + ": " + bytes + " bytes, " + 
+                      count + " allocations" + System.lineSeparator());
+
+        for (String f: funcs) {
+            result.append("  " + f + System.lineSeparator());
+        }
+
+        return result.toString();
+    }
+}
+
+class MallocTraceResult {
+    private ArrayList<Stack> stacks = new ArrayList<>();
+
+    public MallocTraceResult(BufferedReader reader) throws Exception {
+        String line;
+
+        while (true) {
+            line = reader.readLine();
+
+            if (line.startsWith("Stack ")) {
+                break;
+            }
+        }
+
+        while (true) {
+            String[] parts = line.split(":|(bytes,)");
+            long bytes = Long.parseLong(parts[1].trim().split(" ")[0]);
+            long count = Long.parseLong(parts[2].trim().split(" ")[0]);
+            int index = Integer.parseInt(parts[0].trim().split(" ")[1]);
+            int maxIndex = Integer.parseInt(parts[0].trim().split(" ")[3]);
+            stacks.add(new Stack(reader, index, maxIndex, bytes, count));
+            line = reader.readLine();
+
+            if (!line.startsWith("Stack ")) {
+                break;
+            }
+        }
+    }
+    public int nrOfStacks() {
+        return stacks.size();
+    }
+
+    public Stack getStack(int index) {
+        return stacks.get(index);
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        for (Stack s: stacks) {
+            result.append(s.toString());
+        }
+
+        return result.toString();
+    }
+}
+
+
--- test/hotspot/jtreg/runtime/malloctrace/libtestmallochooks.c
+++ test/hotspot/jtreg/runtime/malloctrace/libtestmallochooks.c
@@ -14,53 +14,72 @@
 #define MAX_ALLOC 8192
 #define MAX_CALLOC 64
 
+#define SAFE_PRIME_64 7570865665517081723ull
+#define SAFE_PRIME 1000000007
+
+int next_rand(int last, int base) {
+  return (int) ((((unsigned long long) last) * base) % SAFE_PRIME);
+}
+
 JNIEXPORT void JNICALL
-Java_MallocHooksTest_doRandomMemOps(JNIEnv *env, jclass cls, jint nrOfOps, jint maxLiveAllocations) {
+Java_MallocHooksTest_doRandomMemOps(JNIEnv *env, jclass cls, jint nrOfOps, jint maxLiveAllocations, jint seed) {
     void** roots = calloc(maxLiveAllocations, sizeof(void*));
     int i;
+    int rand = 1;
 
     for (i = 0; i < nrOfOps; ++i) {
-        int idx = (int) (drand48() * maxLiveAllocations);
+        rand = next_rand(rand, seed);
+        int idx = rand % maxLiveAllocations;
 
         if (roots[idx] == NULL) {
-            double r = drand48();
+            rand = next_rand(rand, seed);
+            int what = rand & 31;
+            rand = next_rand(rand, seed);
+            int malloc_size = rand & (MAX_ALLOC - 1);
+            int calloc_size = rand & (MAX_CALLOC - 1);
 
-            if (r < 0.4) {
-                roots[idx] = malloc((int) (drand48() * MAX_ALLOC + 1));
-            } else if (r < 0.75) {
-                roots[idx] = calloc((int) (drand48() * MAX_CALLOC + 1), (int) (drand48() * MAX_CALLOC + 1));
-            } else if (r < 0.80) {
+            if (what < 11) {
+                roots[idx] = malloc(malloc_size + 1);
+            } else if (what < 22) {
+                rand = next_rand(rand, seed);
+                int calloc_count = rand & (MAX_CALLOC - 1);
+                roots[idx] = calloc(calloc_count + 1, calloc_size + 1);
+            } else if (what < 24) {
                 void* mem;
-                int result = posix_memalign(&mem, 64, (int) (drand48() * MAX_ALLOC + 1));
+                int result = posix_memalign(&mem, 64, malloc_size + 1);
 
                 if (result == 0) {
                   roots[idx] = mem;
                 } else {
                   roots[idx] = NULL;
                 }
-            } else if (r < 0.85) {
+            } else if (what < 26) {
 #if !defined(__APPLE__)
-                roots[idx] = memalign(64, (int) (drand48() * MAX_ALLOC + 1));
+                roots[idx] = memalign(64, malloc_size + 1);
 #endif
-            } else if (r < 0.90) {
+            } else if (what < 28) {
 #if !defined(__APPLE__)
-                roots[idx] = aligned_alloc(64, (int) (drand48() * MAX_ALLOC + 1));
+                roots[idx] = aligned_alloc(64, malloc_size + 1);
 #endif
-            } else if (r < 0.95) {
+            } else if (what < 30) {
 #if defined(__GLIBC__) || defined(__APPLE__)
-                roots[idx] = valloc((int) (drand48() * MAX_ALLOC + 1));
+                roots[idx] = valloc(malloc_size + 1);
 #endif
             } else {
 #if defined(__GLIBC__)
-                roots[idx] = pvalloc((int) (drand48() * MAX_ALLOC + 1));
+                roots[idx] = pvalloc(malloc_size + 1);
 #endif
             }
         } else {
-            if (drand48() < 0.8) {
+            rand = next_rand(rand, seed);
+
+            if ((rand & 3) != 0) {
                 free(roots[idx]);
                 roots[idx] = NULL;
             } else {
-                roots[idx] = realloc(roots[idx], (int) (drand48() * MAX_ALLOC + 1));
+                rand = next_rand(rand, seed);
+                int malloc_size = rand & (MAX_ALLOC - 1);
+                roots[idx] = realloc(roots[idx], malloc_size + 1);
             }
         }
     }
