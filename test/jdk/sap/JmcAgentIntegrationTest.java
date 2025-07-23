2e06012f81119e461693b769ee5dc72bf32897e2
Ralf Schmelter <ralf.schmelter@sap.com>
2025-07-23 18:27:43 +0200

    Run at least the VM agnostic tests.

--- test/jdk/sap/JmcAgentIntegrationTest.java
+++ test/jdk/sap/JmcAgentIntegrationTest.java
@@ -2,7 +2,7 @@
  * @test
  * @summary Runs the test for the jcm agent integration.
  *
- * @run main/othervm -Dcp=agent-1.0.1-SNAPSHOT-sap-tests.jar TestJmcAgentIntegration
+ * @run main/othervm -Dcp=agent-1.0.1-SNAPSHOT-sap-tests.jar JmcAgentIntegrationTest
  */
 
 import java.lang.reflect.Method;
@@ -14,7 +14,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-public class TestJmcAgentIntegration {
+public class JmcAgentIntegrationTest {
 
     public static void main(String[] args) throws Exception {
         File jar = new File(System.getenv("TEST_IMAGE_DIR") + "/jars/agent-tests.jar");
@@ -23,17 +23,20 @@ public class TestJmcAgentIntegration {
             return; // Feature was not enabled.
         }
 
+        ArrayList<String> testOptions = new ArrayList<>();
+        testOptions.add("-dump");
+
         // Don't run tests if the ASM version we are using cannot handle the class file spec of this VM.
         int spec = Integer.getInteger("java.vm.specification.version", 99999);
         String javaHome = System.getProperty("java.home");
         File agentJar = new File(javaHome + "/lib/agent.jar");
-        URLClassLoader agentLoader = new URLClassLoader(new URL[] {agentJar.toURI().toURL()},
-                                                        TestJmcAgentIntegration.class.getClassLoader());
+		ClassLoader parentLoader = JmcAgentIntegrationTest.class.getClassLoader();
+        URLClassLoader agentLoader = new URLClassLoader(new URL[] {agentJar.toURI().toURL()}, parentLoader);
         try {
              Class.forName("org.openjdk.jmc.internal.org.objectweb.asm.Opcodes", true, agentLoader).getDeclaredField("V" + spec);
         } catch (NoSuchFieldException e) {
-            System.out.println("Incompatible class file version. Skipping test.");
-            return;
+            System.out.println("Incompatible class file version. Skipping VM specific tests.");
+            testOptions.add("-vm-agnostic-tests");
         }
 
         URL url = jar.toURI().toURL();
@@ -43,9 +46,9 @@ public class TestJmcAgentIntegration {
         System.setProperty("useJmcAgentOption", "true");
         System.setProperty("traceExecs", "true");
 
-        URLClassLoader cl = new URLClassLoader(new URL[] {url}, TestJmcAgentIntegration.class.getClassLoader());
+        URLClassLoader cl = new URLClassLoader(new URL[] {url}, parentLoader);
         Class<?> testClass = Class.forName("org.openjdk.jmc.agent.sap.test.TestRunner", true, cl);
         Method mainMethod = testClass.getDeclaredMethod("main", String[].class, String[].class);
-        mainMethod.invoke(null, new Object[] {new String[] {"-dump"}, new String[]{"-XX:+EnableDynamicAgentLoading"}});
+        mainMethod.invoke(null, new Object[] {testOptions.toArray(new String[0]), new String[] {"-XX:+EnableDynamicAgentLoading"}});
     }
 }
