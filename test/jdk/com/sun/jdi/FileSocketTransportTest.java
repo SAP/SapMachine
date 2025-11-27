4ad8862844bc9bd1294324912c276ff98e481b0f
Ralf Schmelter <ralf.schmelter@sap.com>
2025-11-27 17:38:50 +0100

    Update FileSocketTransportTest to newest version. (#2118)

--- test/jdk/com/sun/jdi/FileSocketTransportTest.java
+++ test/jdk/com/sun/jdi/FileSocketTransportTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2024 SAP SE. All rights reserved.
+ * Copyright (c) 2018, 2025 SAP SE. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -62,6 +62,8 @@ public class FileSocketTransportTest {
         }
         ByteBuffer in = ByteBuffer.wrap(reply);
         int result = channel.read(in);
+        // When we are connected and have received at least one byte, the file should be deleted.
+        checkSocketDeleted(path);
         channel.close();
 
         return result;
@@ -97,8 +99,7 @@ public class FileSocketTransportTest {
             try (SocketChannel channel = SocketChannel.open(StandardProtocolFamily.UNIX)) {
                 // Just see if we can create a unix domain socket on Windows.
             } catch (UnsupportedOperationException e) {
-                System.out.println("Windows version is too old to support unix domain sockets.");
-                return;
+                throw new jtreg.SkippedException("Windows version is too old to support unix domain sockets.");
             }
         }
 
@@ -135,10 +136,9 @@ public class FileSocketTransportTest {
             for (int i = 0; i < 3; ++i) {
                 System.out.println("Run " + i);
                 // Wait a bit to let the debugging be set up properly.
-                Thread.sleep(3000);
+                Thread.sleep(1000);
                 checkSocketPresent(socketName);
                 read = handshake(socketName, handshake, received);
-                checkSocketDeleted(socketName);
                 assertEquals(new String(handshake, "UTF-8"),
                              new String(received, "UTF-8"));
                 assertEquals(read, received.length);
@@ -153,8 +153,16 @@ public class FileSocketTransportTest {
         }
     }
 
-    private static void checkSocketPresent(String name) {
+    private static void checkSocketPresent(String name) throws InterruptedException {
         if (!Platform.isWindows()) {
+            for (int i = 0; i < 10; ++i) {
+                if (!new File(name).exists()) {
+                    Thread.sleep(1000);
+                } else {
+                    break;
+                }
+            }
+
             assertTrue(new File(name).exists(), "Socket " + name + " missing");
         }
     }
