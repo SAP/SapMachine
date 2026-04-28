541e34359383be562926100b482538072fb64453
Ralf Schmelter <ralf.schmelter@sap.com>
2026-04-28 13:49:17 +0200

    SapMachine #2218: Add method to check if a path in zipfs is a symbolic link
    (cherry picked from commit 8ef3761572b2cb0572ba137f139182446e8a0c24)

--- src/java.base/share/classes/jdk/internal/access/SharedSecrets.java
+++ src/java.base/share/classes/jdk/internal/access/SharedSecrets.java
@@ -93,6 +93,8 @@ public class SharedSecrets {
     private static JavaxCryptoSealedObjectAccess javaxCryptoSealedObjectAccess;
     private static JavaxCryptoSpecAccess javaxCryptoSpecAccess;
     private static JavaTemplateAccess javaTemplateAccess;
+	// SapMachine 2026-04-14: Support for symlink detection in zipfs.
+	private static JdkNioZipfsAccess jdkNioZipfsAccess;
 
     public static void setJavaUtilCollectionAccess(JavaUtilCollectionAccess juca) {
         javaUtilCollectionAccess = juca;
@@ -569,5 +571,15 @@ public class SharedSecrets {
         try {
             MethodHandles.lookup().ensureInitialized(c);
         } catch (IllegalAccessException e) {}
+    }
+
+    // SapMachine 2026-04-14
+    public static void setJdkNioZipfsAccess(JdkNioZipfsAccess access) {
+        jdkNioZipfsAccess = access;
+    }
+
+    // SapMachine 2026-04-14
+    public static JdkNioZipfsAccess getJdkNioZipfsAccess() {
+        return jdkNioZipfsAccess;
     }
 }
