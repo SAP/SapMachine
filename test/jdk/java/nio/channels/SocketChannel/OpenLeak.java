3dafe0ac667b98806fee141a3114bc57b19d9861
Christoph Langer <christoph.langer@sap.com>
2026-09-21 09:21:47 +0200

    SapMachine #2416: Timeouts in test java/nio/channels/SocketChannel/OpenLeak.java

--- test/jdk/java/nio/channels/SocketChannel/OpenLeak.java
+++ test/jdk/java/nio/channels/SocketChannel/OpenLeak.java
@@ -21,13 +21,14 @@
  * questions.
  */
 
+// SapMachine 2026-09-21 Disable extended socket exception for this test to avoid timeouts
 /* @test
  * @bug 6548464
  * @summary SocketChannel.open(SocketAddress) leaks file descriptor if
  *     connection cannot be established
  * @requires vm.flagless
  * @build OpenLeak
- * @run junit/othervm OpenLeak
+ * @run junit/othervm -Djdk.includeInExceptions= OpenLeak
  */
 
 import java.io.IOException;
