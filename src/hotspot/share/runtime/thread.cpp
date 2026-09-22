017b66916d2a40490659af3fbd8dd1a88ba8e962
Ralf Schmelter <ralf.schmelter@sap.com>
2026-09-22 16:41:53 +0200

    SapMachine #2392: Add load average to vitals (#2424)

--- src/hotspot/share/runtime/thread.cpp
+++ src/hotspot/share/runtime/thread.cpp
@@ -3107,6 +3107,9 @@ jint Threads::create_vm(JavaVMInitArgs* args, bool* canTryAgain) {
     sapmachine_vitals::initialize_himem_report_facility();
   }
 #endif // LINUX
+  if (EnableVitals) {
+      sapmachine_vitals::begin_sampling();
+  }
 
   BiasedLocking::init();
 
